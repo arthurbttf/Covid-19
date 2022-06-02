c350ae5af5300743e3f92e527ae8735fd2033d8d
Arthur <49170441+arthurbttf@users.noreply.github.com>
2022-06-01 21:01:14 -0300

    Update projeto.py

--- projeto.py
+++ projeto.py
@@ -9,7 +9,7 @@ Original file is located at
 # Bibliotecas do Projeto
 """
 
-!pip install geopandas beautifulsoup4 #urllib3[secure]
+pip install geopandas beautifulsoup4 #urllib3[secure]
 import urllib3, urllib.request, json, pandas as pd, geopandas as gpd, plotly.express as px
 from bs4 import BeautifulSoup
 
